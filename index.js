87099319c7b342a7445d26daa69d5c51589d1925
SleepKnight8 <jehadcoder@gmail.com>
2026-05-10 07:46:35 +0300

    Initialize game variables and betting functionality
    Added variables for win/lose counts, player move, balance, and bet. Implemented BetFunc to adjust the bet amount based on user input.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let win_count;
+let lose_count;
+let player_move;
+let balance = 50000;
+let bet = 1000;
+
 function chooseMove(button) {
 
     document.querySelectorAll(".PlayerMove").forEach(btn => {
@@ -5,4 +11,20 @@ function chooseMove(button) {
     });
 
     button.style.backgroundColor = "lightblue";
+
+    player_move = button.value;
+    // console.log(player_move); debug OK
+}
+
+function BetFunc(button) {
+    if (button.id === "Increase") {
+        if (bet <= balance-1000) {
+            bet += 1000;
+        }
+    }
+    else if (button.id === "Decrease" && bet !== 1000) {
+        bet -= 1000;
+    }
+
+    document.getElementById("BetDisplay").innerHTML = bet;
 }
