b88c81be0badac57a1777949cf84e8e35615baf1
SleepKnight8 <jehadcoder@gmail.com>
2026-05-10 13:16:32 +0300

    Refactor play function to handle betting logic

--- index.js
+++ index.js
@@ -38,62 +38,66 @@ function BetFunc(button) {
 }
 
 function play() {
-
-    if (balance <= 0) {
-        alert("You Lost the Game!");
-        return;
-    }
-
-    const moves = ["R", "P", "S"];
-
-    const computer_move_random = moves[Math.floor(Math.random() * 3)];
-
-    document.getElementById("ComputerMoveDisplay").innerHTML = computer_move_icon[computer_move_random];
-
-    if (computer_move_random === player_move) {
-        document.getElementById("result").innerHTML = "Tie";
-        document.getElementById("BetResult").innerHTML = "0";
-        document.getElementById("BetResult").style.color = "lightslategray";
-        saveGame();
-        return;
-    }
-
-    const BetWin =
-        (player_move === "R" && computer_move_random === "S") ||
-        (player_move === "P" && computer_move_random === "R") ||
-        (player_move === "S" && computer_move_random === "P");
-
-    if (BetWin) {
-        document.getElementById("result").innerHTML = "You Win!";
-        document.getElementById("BetResult").innerHTML = "+" + bet;
-        balance += bet;
-        document.getElementById("BetResult").style.color = "#06d6a0";
-    } else {
-        document.getElementById("result").innerHTML = "You Lose!";
-        document.getElementById("BetResult").innerHTML = "-" + bet;
-        balance -= bet;
-        document.getElementById("BetResult").style.color = "#ef476f";
-
-
-    }
-
-    document.getElementById("MoneyDisplay").innerHTML = balance;
-    document.getElementById("WinStatus").innerHTML = "Wins: " + win_count;
-    document.getElementById("LoseStatus").innerHTML = "Loses: " + lose_count;
-
-    if (balance >= 1000000) {
-        alert("You Win the Game!");
-        win_count += 1;
-        NewGame();
-    }
-
-    else if (balance <= 0) {
-        alert("You Lost the Game!");
-        lose_count += 1;
-        NewGame();
-    }
-
+    if (bet > balance) {
+        if (balance <= 0) {
+            alert("You Lost the Game!");
+            return;
+        }
+    
+        const moves = ["R", "P", "S"];
+    
+        const computer_move_random = moves[Math.floor(Math.random() * 3)];
+    
+        document.getElementById("ComputerMoveDisplay").innerHTML = computer_move_icon[computer_move_random];
+    
+        if (computer_move_random === player_move) {
+            document.getElementById("result").innerHTML = "Tie";
+            document.getElementById("BetResult").innerHTML = "0";
+            document.getElementById("BetResult").style.color = "lightslategray";
+            saveGame();
+            return;
+        }
+    
+        const BetWin =
+            (player_move === "R" && computer_move_random === "S") ||
+            (player_move === "P" && computer_move_random === "R") ||
+            (player_move === "S" && computer_move_random === "P");
+    
+        if (BetWin) {
+            document.getElementById("result").innerHTML = "You Win!";
+            document.getElementById("BetResult").innerHTML = "+" + bet;
+            balance += bet;
+            document.getElementById("BetResult").style.color = "#06d6a0";
+        } else {
+            document.getElementById("result").innerHTML = "You Lose!";
+            document.getElementById("BetResult").innerHTML = "-" + bet;
+            balance -= bet;
+            document.getElementById("BetResult").style.color = "#ef476f";
+    
+    
+        }
+    
+        document.getElementById("MoneyDisplay").innerHTML = balance;
+        document.getElementById("WinStatus").innerHTML = "Wins: " + win_count;
+        document.getElementById("LoseStatus").innerHTML = "Loses: " + lose_count;
+    
+        if (balance >= 1000000) {
+            alert("You Win the Game!");
+            win_count += 1;
+            NewGame();
+        }
+    
+        else if (balance <= 0) {
+            alert("You Lost the Game!");
+            lose_count += 1;
+            NewGame();
+        }
+        
+    else{
+        bet = balance;
+        }    
     saveGame();
+    }
 }
 function saveGame() {
     localStorage.setItem("balance", balance);
